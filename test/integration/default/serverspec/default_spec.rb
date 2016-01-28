046156c22ec843a1740410f89bd61f8cbfec02f3
John Doe <jdoe@example>
2016-01-28 21:47:16 +0000

    Kitchen Test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe 'workstation::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  
+  describe package ('tree') do
+    it { should be_installed }
   end
+
+  describe file ('/etc/motd') do
+    it { should be_owned_by 'root' }
+  end
+
 end
